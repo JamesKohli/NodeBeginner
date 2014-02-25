cb4cad73a7a771e14569dfde74d5ef32eed5927d
Jkohli <jkohli@g2ft.com>
2014-02-25 17:02:53 -0500

    now a functional server w/ url parsing

---
+++ server.js
@@ -0,0 +1,16 @@
+var http = require('http');
+var url = require("url");
+
+function start() {
+	http.createServer(function(req, res) {
+		var pathname = url.parse(req.url).pathname;
+		console.log("Request for " + pathname + " received");
+		res.writeHead(200, {"Content-Type": "text/plain"});
+		res.write("Hello world");
+		res.end();
+	}).listen(8888);
+	
+	console.log("Server started")
+}
+
+exports.start = start;
